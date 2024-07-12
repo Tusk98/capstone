2d2177baee63afade42f3f340add7a85f7a53c48
HenryW <henryracoomb@gmail.com>
2024-07-12 10:32:29 -0400

    Converted generate data to JSON

--- gen_data.py
+++ gen_data.py
@@ -1,6 +1,4 @@
-import csv
 import random
-from datetime import datetime, timedelta
 import json
 
 # Searchable Enterprise Directory 
@@ -8,24 +6,22 @@ import json
 names = ["One", "Two", "Three", "Four", "Five"]
 job_titles = ["Software", "Hardware", "Frontware", "Backware", "Middleware"]
 work_locale = ["WFH", "Toronto", "North Pole", "South Pole"]
-NUM_ROWS = 1000
+NUM_ROWS = 1000 
 
-OUTPUT_FILE = "generated_Proj_data.csv"
+OUTPUT_FILE = "generated_proj_data.json" 
 
-def generate_job_data():
+def generate_job_data(): 
     return {
-        "Name": random.choice(names),
-        "Phone_number": random.randint(1111111111, 9999999999),
-        "Job_role": random.choice(job_titles),
-        "Work_Location": random.choice(work_locale),
+        "Name": random.choice(names), 
+        "Phone_number": random.randint(1111111111, 9999999999), 
+        "Job_role": random.choice(job_titles), 
+        "Work_Location": random.choice(work_locale), 
         "Salary": random.randint(5, 20) * 10000
     }
 
-if __name__ == "__main__":
-    with open(OUTPUT_FILE, mode='w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=["Name", "Phone_number", "Job_role", "Work_Location", "Salary"])
-        writer.writeheader()
-        for _ in range(NUM_ROWS):
-            writer.writerow(generate_job_data())
+data_rows = [generate_job_data() for _ in range(NUM_ROWS)]
 
-    print(f"Data has been written to {OUTPUT_FILE}")
+with open(OUTPUT_FILE, "w") as file:
+    json.dump(data_rows, file, indent=4)
+
+print("Data generation completed")
