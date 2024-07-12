f3f50e7a3f0a58843ce7d38bb731f26fd7382268
HenryW <henryracoomb@gmail.com>
2024-07-12 13:37:39 -0400

    Regenerated dummy data with employee_id, and normally distributed salary

--- gen_data.py
+++ gen_data.py
@@ -9,20 +9,18 @@ work_locale = ["WFH", "Toronto", "North Pole", "South Pole"]
 NUM_ROWS = 1000 
 
 OUTPUT_FILE = "generated_proj_data.json" 
-count = 0
 
-def generate_job_data(): 
-    count += 1 
+def generate_job_data(i): 
     return {
-        "Employee_id": count,
+        "Employee_id": i+1,
         "Name": random.choice(names), 
         "Phone_number": random.randint(1111111111, 9999999999), 
         "Job_role": random.choice(job_titles), 
         "Work_location": random.choice(work_locale), 
-        "Salary": random.randint(5, 20) * 10000
+        "Salary": int(random.gauss(100000, 15000))
     }
 
-data_rows = [generate_job_data() for _ in range(NUM_ROWS)]
+data_rows = [generate_job_data(i) for i in range(NUM_ROWS)]
 
 with open(OUTPUT_FILE, "w") as file:
     json.dump(data_rows, file, indent=4)
