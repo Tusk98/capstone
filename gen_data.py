dc803fa502705e09aaa239e9441dfa10362e0886
HenryW <henryracoomb@gmail.com>
2024-07-12 13:20:42 -0400

    Fixed minor typo in data generation

--- gen_data.py
+++ gen_data.py
@@ -9,13 +9,16 @@ work_locale = ["WFH", "Toronto", "North Pole", "South Pole"]
 NUM_ROWS = 1000 
 
 OUTPUT_FILE = "generated_proj_data.json" 
+count = 0
 
 def generate_job_data(): 
+    count += 1 
     return {
+        "Employee_id": count,
         "Name": random.choice(names), 
         "Phone_number": random.randint(1111111111, 9999999999), 
         "Job_role": random.choice(job_titles), 
-        "Work_Location": random.choice(work_locale), 
+        "Work_location": random.choice(work_locale), 
         "Salary": random.randint(5, 20) * 10000
     }
 
