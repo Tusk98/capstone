f89c5b937eec803ba04f7fb5b32b4edac9f3b9c8
Alissia Rugina <alissiarugina@gmail.com>
2024-07-12 10:43:09 -0400

    connected to mongodb

--- server/server.js
+++ server/server.js
@@ -1,8 +1,29 @@
 import express from 'express';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
 
 const app = express();
 const PORT = 3000;
 
+
+app.get('/employees', async (_req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const employees = await collection.find({}).toArray();
+        res.json(employees);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Error, /employees not reached!");
+    }
+});
 
 app.get('/test', async (_req, res) => {
     try {
